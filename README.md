e29fa6f344432a74ee7bb5721c462395d5a9e95b
Ex-iT <jdijkstra@imacrd1003.reshift.local>
2018-01-23 13:44:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Stream grabber
+
+Fetches the current XML stream data from the Slam Non-stop live stream and writes it to an M3U file in the current user /Documents/ folder.
+
+## Usage
+
+Install dependencies with `npm install` and run:
+
+```
+$ node index.js
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const parser = require('xml2json');
+
+const url = 'https://playerservices.streamtheworld.com/api/livestream?station=web10&transports=http%2Chls%2Chlsts&version=1.9&request.preventCache=1516699625968';
+const outputFile = path.join(process.env.HOME || process.env.USERPROFILE, 'Documents', 'Slam-non-stop.m3u');
+
+fetch(url)
+    .then(response => response.text())
+    .then(xml => JSON.parse(parser.toJson(xml)))
+    .then(json => {
+        const protocol = 'https';
+        const mountpoint = json.live_stream_config.mountpoints.mountpoint[0];
+        const servers = mountpoint.servers.server;
+        const mount = mountpoint.mount;
+        const urls = servers.map(server => `${protocol}://${server.ip}/${mount}`).join('\n');
+
+        writeM3UFile(urls, outputFile);
+    })
+    .catch(err => console.log(err));
+
+function writeM3UFile(data, path) {
+    const playlistPrefix = '#EXTM3U\n';
+    fs.writeFile(outputFile, playlistPrefix + data, err => {
+        if (err) { 
+            console.log(`[-] An error occured: ${err}`);
+            return;
+        }
+        console.log(`[+] Playlist saved to: ${outputFile}`);
+    }); 
+}
