c084e072425078a7a5facb063a8c1c71746aa0da
Ex-iT <vic.toothpick@gmail.com>
2018-05-09 17:10:38 +0200

    Fixed write file func to actually use the path param and converted spaces to tabs

--- index.js
+++ index.js
@@ -8,25 +8,25 @@ const author = 'SLAM! - Non-Stop';
 const outputFile = path.join(process.env.HOME || process.env.USERPROFILE, 'Documents', 'Slam-non-stop.m3u');
 
 fetch(url)
-    .then(response => response.text())
-    .then(xml => JSON.parse(parser.toJson(xml)))
-    .then(json => {
-        const mountpoint = json.live_stream_config.mountpoints.mountpoint[0];
-        const servers = mountpoint.servers.server;
-        const mount = mountpoint.mount;
-        const urls = servers.map(server => `#EXTINF:,${author} (${server.sid})\n${server.ports.port.type}://${server.ip}/${mount}`).join('\n');
+	.then(response => response.text())
+	.then(xml => JSON.parse(parser.toJson(xml)))
+	.then(json => {
+		const mountpoint = json.live_stream_config.mountpoints.mountpoint[0];
+		const servers = mountpoint.servers.server;
+		const mount = mountpoint.mount;
+		const urls = servers.map(server => `#EXTINF:,${author} (${server.sid})\n${server.ports.port.type}://${server.ip}/${mount}`).join('\n');
 
-        writeM3UFile(urls, outputFile);
-    })
-    .catch(err => console.log(`[-] An error occured fetching the data: ${err}`));
+		writeM3UFile(urls, outputFile);
+	})
+	.catch(err => console.log(`[-] An error occured fetching the data: ${err}`));
 
 function writeM3UFile(data, path) {
-    const playlistPrefix = '#EXTM3U\n';
-    fs.writeFile(outputFile, playlistPrefix + data, err => {
-        if (err) { 
-            console.log(`[-] An error occured writing the file: ${err}`);
-            return;
-        }
-        console.log(`[+] Playlist saved to: ${outputFile}`);
-    }); 
+	const playlistPrefix = '#EXTM3U\n';
+	fs.writeFile(path, playlistPrefix + data, err => {
+		if (err) {
+			console.log(`[-] An error occured writing the file: ${err}`);
+			return;
+		}
+		console.log(`[+] Playlist saved to: ${outputFile}`);
+	});
 }
